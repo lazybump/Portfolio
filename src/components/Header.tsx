b7835068ea083be265b5ca1174003cd64a918214
lazybump <104283344+lazybump@users.noreply.github.com>
2023-07-01 15:07:44 +0100

    style heading on larger screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,35 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 
 const Header = () => {
+  const navItems = ["Projects", "Testimonials", "FAQs"];
   return (
     <header
-      className="fixed z-10 w-full top-0 flex items-center justify-between px-4 py-2 md:px-8 md:h-24 bg-opacity-70 bg-clip-padding"
+      className="fixed z-10 w-full top-0 flex items-center justify-between px-4 py-2 md:px-16 lg:px-28 lg:py-4 bg-opacity-70 bg-clip-padding"
       style={{ backdropFilter: "blur(3px)", WebkitBackdropFilter: "blur(3px)" }}
     >
       <div>
         <span className="font-bold text-4xl text-primary">an</span>
       </div>
-      <div className="hidden lg:block">
-        <nav>
-          <ul>
-            <li>Projects</li>
-            <li>Testimonials</li>
-            <li>FAQs</li>
-          </ul>
-        </nav>
-        <button>Get in touch</button>
-      </div>
+      <nav className="hidden lg:block w-[500px]">
+        <ul className="text-gray-400 flex justify-between items-center">
+          {navItems.map((item, index) => (
+            <li key={index}>
+              <a
+                href="#"
+                className="hover:text-white duration-300 relative group"
+              >
+                {item}
+                <span className="bg-primary absolute -bottom-4 h-0.5 left-1/2 -translate-x-1/2 w-1 transition-all duration-300 group-hover:w-full"></span>
+              </a>
+            </li>
+          ))}
+          <button className="bg-white text-black px-4 py-2 rounded font-medium text-lg">
+            Get in touch
+          </button>
+        </ul>
+      </nav>
 
-      <div>
+      <div className="lg:hidden">
         <RxHamburgerMenu size={28} className="stroke-1 text-primary" />
       </div>
     </header>
